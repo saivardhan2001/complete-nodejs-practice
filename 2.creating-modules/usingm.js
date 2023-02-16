781acd1ee5861e06234cf2d5ba19fefcbb471c37
K. Sai Vardhan Reddy <107573686+saivardhan2001@users.noreply.github.com>
2023-02-16 11:33:48 +0530

    Add files via upload

---
+++ 2.creating-modules/usingm.js
@@ -0,0 +1,7 @@
+const use = require("./creatingm");
+console.log(use.add(1,2));
+console.log(use.mul(1,2));
+
+//           OR
+// const {add,sub,mul,div}=require("./creatingm")
+// console.log(add(1,4));
